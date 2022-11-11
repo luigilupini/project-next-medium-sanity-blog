575687baacb720d801fddc4a0796614782495bc7
Luigi Lupini <luigi.lupini@gmail.com>
2022-11-11 08:42:25 +0200

    2# Header component

---
+++ components/Header.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from "../public/static/logo.png";
+// `Link` can prefetch <a> beforehand on clients behalf:
+// `next/link` caches results in the client-side and your visitor will not fetch
+// a revalidated result out of the box unless there is a full-page reload.
+// https://nextjs.org/docs/api-reference/next/link
+export default function Header() {
+  return (
+    <header className="flex justify-between p-5 mx-auto max-w-7xl">
+      <div className="flex items-center space-x-5">
+        <Link href="/">
+          <Image
+            src={logo}
+            width={176}
+            height={176}
+            alt="logo"
+            className="inline-block cursor-pointer"
+          />
+          {/* <span className="text-xs font-bold">plus</span> */}
+        </Link>
+        {/* Mobile first with the div hidden till we breakpoint into flex */}
+        <div className="items-center hidden space-x-5 md:inline-flex">
+          <h3>About</h3>
+          <h3>Contact</h3>
+          <h3 className="px-4 py-1 text-white bg-green-600 rounded-full">
+            Follow
+          </h3>
+        </div>
+      </div>
+      <div className="flex items-center space-x-5 text-green-600">
+        <h3>Sign In</h3>
+        <h3 className="px-4 py-1 border border-green-600 rounded-full">
+          Get Started
+        </h3>
+      </div>
+    </header>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
         <title>Medium+</title>
         <link rel="icon" href="/public/favicon.ico" />
       </Head>
+      <Header />
     </div>
   );
 }
